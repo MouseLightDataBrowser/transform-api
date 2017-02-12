08870a19f1853c29346524f882d337fdc8b6b2c2
Patrick Edson <pedson@leapscientific.com>
2017-02-11 21:33:08 -0500

    Clean up

--- src/graphql/serverContext.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const debug = require("debug")("ndb:transform:context");
-class GraphQLServerContext {
-}
-exports.GraphQLServerContext = GraphQLServerContext;
-//# sourceMappingURL=serverContext.js.map
