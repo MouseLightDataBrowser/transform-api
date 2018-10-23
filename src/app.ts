3e49efc6a70c4bd8848a677a35b6e48dfb5cc07b
Patrick Edson <pedson@users.noreply.github.com>
2018-10-23 10:36:27 -0400

    Remove unused.

--- src/app.ts
+++ src/app.ts
@@ -18,36 +18,4 @@ server.applyMiddleware({app, path: ServiceOptions.graphQLEndpoint});
 
 app.use("/tracings", tracingQueryMiddleware);
 
-app.listen(ServiceOptions.port, () => debug(`transform api server is now running on http://${os.hostname()}:${ServiceOptions.port}`));
-
-/*
-import * as express from "express";
-import * as os from "os";
-import {createServer} from "http";
-import * as bodyParser from "body-parser";
-
-const debug = require("debug")("mnb:transform:server");
-
-import {ServiceOptions} from "./options/serviceOptions";
-
-import {graphQLMiddleware, graphiQLMiddleware} from "./graphql/middleware/graphQLMiddleware";
-import {tracingQueryMiddleware} from "./rawquery/tracingQueryMiddleware";
-
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(bodyParser.json());
-
-app.use("/tracings", tracingQueryMiddleware);
-
-app.use(ServiceOptions.graphQLEndpoint, graphQLMiddleware());
-
-app.use(["/", ServiceOptions.graphQLEndpoint], graphiQLMiddleware(ServiceOptions));
-
-const server = createServer(app);
-
-server.listen(ServiceOptions.port, () => {
-    debug(`transform api server is now running on http://${os.hostname}:${ServiceOptions.port}`);
-});
-*/
+app.listen(ServiceOptions.port, () => debug(`transform api server is now running on http://${os.hostname()}:${ServiceOptions.port}/graphql`));
